6ab2889b6de71e2d9aac55e5e3b7964a9bd02c1c
SantiagoF03 <102191455+SantiagoF03@users.noreply.github.com>
2023-06-05 21:39:13 -0300

    Revert "aa"
    This reverts commit 31a6e31c897f7be90e7df0f21772d1ce6bed1375, reversing
    changes made to ee5f3f1983c441de8ebb7dc87de61c0746355e65.

--- Assets/Script/InputManager/KartController.cs
+++ Assets/Script/InputManager/KartController.cs
@@ -12,9 +12,9 @@ public class KartController : MonoBehaviour
 
     #region Stats
 
-    private float SpeedStats => kartEntity.GetKartSpeed();
+    private float SpeedStats => kartEntity.Speed;
 
-    private float SteerDirStats => kartEntity.GetKartSpeedRotate();
+    private float SteerDirStats => kartEntity.SpeedRotate;
 
     #endregion
     
@@ -32,12 +32,7 @@ public class KartController : MonoBehaviour
         private float driftTime;
         public float minSpeedToCrash;
         public float realSpeed; //not the applied speed
-<<<<<<< HEAD
         public float maxSpeed; //max possible speed
-=======
-        public float minSpeedToCrash; //not the applied speed
-        public float maxSpeed => SpeedStats; //max possible speed
->>>>>>> main
         public float boostSpeed; //speed while boosting
         public Vector3 jumpDirection = new Vector3(0,0,1); //direction in which the kart will jump, strictly up for now
     
@@ -59,14 +54,9 @@ public class KartController : MonoBehaviour
     private bool Choco = false;
     private void Start()
     {
-<<<<<<< HEAD
         // Set Kart Stats
         maxSpeed = SpeedStats;
         steerDirection = SteerDirStats;
-=======
-        
-        
->>>>>>> main
         StartVelocity = maxSpeed;
         //rb = GetComponent<Rigidbody>();
         hasInputManager = _inputManager;
