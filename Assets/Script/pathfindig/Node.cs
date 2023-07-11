4cd6e9f82f71c67adc07299acb6638c27477eb31
SantiagoF03 <102191455+SantiagoF03@users.noreply.github.com>
2023-07-11 20:02:30 -0300

    Revert "Merge branch 'santi_backUp' into santi_nuevaRama"
    This reverts commit cba87f7f7c34fe7f4a1bbfe1d7eb5ef4dcd91a70, reversing
    changes made to 4b00162579451e3f31574a451bd03744057aea39.

--- Assets/Script/pathfindig/Node.cs
+++ Assets/Script/pathfindig/Node.cs
@@ -9,34 +9,9 @@ public class Node : MonoBehaviour
     public GameObject ParentNodes;
     public Node[] NeighboarNodes = new Node[5];
 
-    
+
     private void Awake()
     {
-        string nombreCompleto = gameObject.name;
-
-        // Extraer una parte del nombre del objeto
-        if (nombreCompleto.Length == 10)
-        {
-            String m = nombreCompleto.Substring(6, 3);
-
-            ID = int.Parse(m);
-        }
-        if (nombreCompleto.Length == 9) 
-        {
-            String m = nombreCompleto.Substring(6, 2);
-
-            ID = int.Parse(m);
-        }
-        if (nombreCompleto.Length == 8)
-        {
-            String m = nombreCompleto.Substring(6, 1);
-
-            ID = int.Parse(m);
-        }
-        if (nombreCompleto.Length <= 7) 
-        {
-            ID = 0;
-        }
     }
 
 }
