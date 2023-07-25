0350a66e72e732e61982fbb4e5e154c82b0553af
SantiagoF03 <102191455+SantiagoF03@users.noreply.github.com>
2023-07-25 01:11:05 -0300

    a

--- Assets/Script/Powers/KartPowerPickUp.cs
+++ Assets/Script/Powers/KartPowerPickUp.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class KartPowerPickUp : MonoBehaviour, IOptimizatedUpdate
 {
+    public PowerController aiController;
     public RuletaPoderes powerRoulette;
     private bool hasPower = false;
     private GameObject selectedPower;
@@ -16,33 +17,54 @@ public class KartPowerPickUp : MonoBehaviour, IOptimizatedUpdate
     public Transform BackPowerPos3;
     Vector3 destination;
 
+    public GameObject SelectedPower
+    {
+        get { return selectedPower; }
+        set { selectedPower = value; }
+    }
+
+    // Propiedad para verificar si el kart tiene un poder
+    public bool HasPower
+    {
+        get { return hasPower; }
+        set { hasPower = value; }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PowerBox"))
         {
             if (!hasPower)
             {
-                // Girar la ruleta y obtener el poder seleccionado
-                selectedPower = powerRoulette.GirarRuleta();
+                if (aiController != null)
+                {
+                    // Si hay un controlador de IA, dejar que tome la decisión
+                    Debug.Log("poderIA");
 
-                hasPower = true;
+                    aiController.DecideAction(gameObject, powerRoulette);
+                }
+                else
+                {
+                    // Si no hay un controlador de IA, el jugador toma la decisión
+                    selectedPower = powerRoulette.GirarRuleta();
+                    hasPower = true;
+                    Debug.Log(selectedPower);
+                }
             }
         }
     }
 
-    private void ActivatePower()
+    public void ActivatePower()
     {
-
         if (selectedPower.CompareTag("IceWall"))
         {
-           GameObject _risingWallPrefab = Instantiate(risingWallPrefab, BackPowerPos.position, BackPowerPos.rotation);
+            GameObject _risingWallPrefab = Instantiate(risingWallPrefab, BackPowerPos.position, BackPowerPos.rotation);
             _risingWallPrefab.GetComponent<IceWall>().Owner = this.gameObject;
         }
         else if (selectedPower.CompareTag("Mug"))
         {
             GameObject _mug = Instantiate(mug, BackPowerPos2.position, BackPowerPos2.rotation);
             _mug.GetComponent<SlowZone>().Owner = this.gameObject;
-
         }
         else if (selectedPower.CompareTag("aceleration"))
         {
@@ -55,9 +77,7 @@ public class KartPowerPickUp : MonoBehaviour, IOptimizatedUpdate
     {
         if (hasPower && Input.GetKey(KeyCode.P))
         {
-            Debug.Log("entre");
             ActivatePower();
-
             hasPower = false;
         }
     }
