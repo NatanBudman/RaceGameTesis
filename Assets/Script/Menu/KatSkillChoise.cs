15152e285118c8df828828e5fadd3fc7d201fc0e
NatanBudman <91756746+NatanBudman@users.noreply.github.com>
2023-09-05 13:24:08 -0300

    se

--- Assets/Script/Menu/KatSkillChoise.cs
+++ Assets/Script/Menu/KatSkillChoise.cs
@@ -5,20 +5,40 @@ using UnityEngine.UI;
 
 public class KatSkillChoise : MonoBehaviour
 {
+    public Skills[] skills;
+    
+    [Space]
+    [Space]
+    [Space]
 
     public Color CurrentSkillColor;
     public Color SkillDefaultColor;
 
-
+    public Text SkillDescription;
     public Image CurrentSkillSelected;
 
-    public void SkillSelected(Image BackgroundSkill) 
+    public void SkillSelected(Image BackgroundSkill)
     {
-        if (CurrentSkillSelected != null) 
+        if (CurrentSkillSelected != null)
         {
             CurrentSkillSelected.color = SkillDefaultColor;
         }
         CurrentSkillSelected = BackgroundSkill;
         BackgroundSkill.color = CurrentSkillColor;
+
     }
+
+    public void SkillSelected(int i) 
+    {
+        SkillDescription.text = skills[i].Description;
+    }
+}
+
+[System.Serializable]
+public struct Skills 
+{
+    public string SkillName;
+    [TextArea]
+    public string Description;
+
 }
