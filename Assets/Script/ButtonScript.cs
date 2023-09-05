e15120b43198b0b1eb56dcea549ed9d1a1bfdb4a
SantiagoF03 <102191455+SantiagoF03@users.noreply.github.com>
2023-09-05 01:21:21 -0300

    Revert "Merge branch 'main' into santi_BackUpNuevaRama"
    This reverts commit 2e0bb3859d8913bea28b7a87f0658484edaad67e, reversing
    changes made to aefa57e7cce0f995050268af2ee341542208eb7c.

--- Assets/Script/ButtonScript.cs
+++ Assets/Script/ButtonScript.cs
@@ -5,32 +5,17 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-
-    public AudioSource confirm;
-    public AudioSource confirm2;
-    public AudioSource confirm3;
-    public AudioSource back;
-    public AudioSource error;
-    public AudioSource kart_motor;
-    public AudioSource select;
-    public AudioSource choose;
-    public AudioSource options;
-    public AudioSource credits;
-    public AudioSource chooseSkill;
-    public AudioSource selectamap;
-    public AudioSource ready;
-
     private void Start()
     {
         Time.timeScale = 1;
     }
     public void EnablePanel(GameObject Panel)
     {
-        Panel.SetActive(true);   
+        Panel.SetActive(true);
     }
     public void DisablePanel(GameObject Panel)
     {
-        Panel.SetActive(false); 
+        Panel.SetActive(false);
     }
 
     public void ExitGame()
@@ -42,70 +27,4 @@ public class ButtonScript : MonoBehaviour
     {
         SceneManager.LoadScene($"Scenes/{SceneName}");
     }
-
-    public void PlaySoundConfirm()
-    {
-        confirm.Play();
-    }
-
-    public void PlaySoundConfirm2()
-    {
-        confirm2.Play();
-    }
-
-    public void PlaySoundConfirm3()
-    {
-        confirm3.Play();
-    }
-
-    public void PlaySoundKartMotor()
-    {
-        kart_motor.Play();
-    }
-
-    public void PlaySoundSelect()
-    {
-        select.Play();
-    }
-
-    public void PlaySoundBack()
-    {
-        back.Play();
-    }
-
-    public void PlaySoundChoose()
-    {
-        choose.Play();
-    }
-
-    public void PlaySoundChooseSkill()
-    {
-        chooseSkill.Play();
-    }
-
-    public void PlaySoundSelectAMap()
-    {
-        selectamap.Play();
-    }
-
-    public void PlaySoundOptions()
-    {
-        options.Play();
-    }
-
-    public void PLaySoundCredits()
-    {
-        credits.Play();
-    }
-
-    public void PlaySoundReady()
-    {
-        ready.Play();
-    }
-
-    public void PlaySoundError()
-    {
-        error.Play();
-    }
-
 }
